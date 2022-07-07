d987f3266a766b6da551d85fd6355f04a4ade4f5
Roland Laboulaye <rlaboulaye@gmail.com>
2022-07-07 10:59:51 -0400

    remove web server approach code

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from flask import Flask
-
-
-app = Flask(__name__)
-# app.secret_key = os.environ.get('SECRET_KEY')
-app.secret_key = "00328e042b1d9a75f86ff9ca547e6a1688b455f6b8a73784"
-from app import routes
